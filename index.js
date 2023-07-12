2ea249e5ad57a5013f12ab4032a80fa0c4b44da8
S00197531 <S00197531@mail.itsligo.ie>
2023-07-12 13:38:44 +0100

    created getComputerChoice function

---
+++ index.js
@@ -0,0 +1,17 @@
+
+function getComputerChoice () {
+    let computerChoices = ['Rock', 'Paper', 'Scissors'];
+
+    let computerSelection = Math.floor(Math.random()*computerChoices.length);
+
+    if (computerSelection == 0){
+        return 'Rock'
+    } else if (computerSelection == 1){
+        return 'Paper'
+    } else {
+        return 'Scissors'
+    }
+
+}
+
+console.log(getComputerChoice())
