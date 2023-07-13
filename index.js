4a1d56a5c979e4bef340e489740be0e761c4c02d
adamsaul314 <adamsaul314@gmail.com>
2023-07-13 11:00:34 +0100

    added play round function. game now works in the console

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let playerSelection = prompt('Choose rock, paper or scissors: ');
+
+let computerSelection = getComputerChoice();
+
 
 function getComputerChoice () {
     let computerChoices = ['Rock', 'Paper', 'Scissors'];
@@ -12,3 +16,40 @@ function getComputerChoice () {
         return 'Scissors'
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+
+
+    if (playerSelection == 'Rock' && computerSelection == 'Paper'){
+        console.log('You lose! Paper beats rock.')
+
+    } else if (playerSelection == 'Rock' && computerSelection == 'Scissors'){
+        console.log('You win! Rock beats scissors.')
+
+    } else if (playerSelection == 'Rock' && computerSelection == 'Rock'){
+        console.log('Tie! Play again.')
+
+    } else if (playerSelection == 'Paper' && computerSelection == 'Paper'){
+        console.log('Tie! Play again.')
+
+    } else if (playerSelection == 'Paper' && computerSelection == 'Scissors'){
+        console.log('You Lose! Scissors beats paper.')
+
+    } else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
+        console.log('You Win! Paper beats rock.')
+
+    } else if (playerSelection == 'Scissors' && computerSelection == 'Paper'){
+        console.log('You Win! Scissors beats paper.')
+
+    }else if (playerSelection == 'Scissors' && computerSelection == 'Scissors'){
+        console.log('Tie! Play again.')
+
+    } else if (playerSelection == 'Scissors' && computerSelection == 'Rock'){
+        console.log('You Lose! Rock beats scissors.')
+    }
+
+}
+
+console.log(computerSelection)
+
+console.log(playRound(playerSelection, computerSelection))
