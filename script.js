3cf91caca668861d1b197ed3eaa27cd8f4f807e6
Adam <adamsaul314@gmail.com>
2025-01-08 09:30:56 +0000

    wrote logic to get computer choice

---
+++ script.js
@@ -0,0 +1,20 @@
+function getComputerChoice(){
+
+    let computerChoice;
+    let computerNum = Math.floor(Math.random() * 3); //returns random int from 0 to 2
+
+    if (computerNum == 0){
+        computerChoice = 'Rock'
+    }
+    else if (computerNum == 1){
+        computerChoice = 'Paper'
+    }
+    else {
+        computerChoice='Scissors'
+    }
+
+    console.log(computerChoice);
+
+
+}
+
